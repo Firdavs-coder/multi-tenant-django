c61f2ab830fb3466883a340a22d97c6cc67027c6
Firdavs <84655445+Firdavs-coder@users.noreply.github.com>
2023-04-22 13:01:01 +0500

    Update settings.py

--- example/settings.py
+++ example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5%6t%kmjf+ab(6qvjdifnxhyf6dadknp7@n&9^l!wvajx4k7@(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", ".localhost"]
+ALLOWED_HOSTS = ["localhost", ".localhost", "*", ".onrender", "onrender"]
 
 
 # Application definition
